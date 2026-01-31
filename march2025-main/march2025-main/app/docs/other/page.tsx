2942c7cb0f52456f955e458200d7993ed1e3ac72
Firefly0618 <taskcsv77@gmail.com>
2026-01-30 21:05:18 -0800

    Delete march2025-main/march2025-main directory

--- march2025-main/march2025-main/app/docs/other/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ArticleOther() {
-  return (
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias debitis
-      repudiandae ullam accusamus ut alias impedit quidem commodi itaque facilis
-      corrupti natus laboriosam ducimus, id numquam, quibusdam quod a ipsum?
-    </p>
-  );
-}
